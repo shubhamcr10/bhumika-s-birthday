542909b1c0dfcce5d037153e0582a5296e340006
ShubhamWindows <shubhamcr10@gmail.com>
2025-12-27 03:13:29 +0530

    test to fix the black screen issue

--- src/components/CakeScene/CakeScene.jsx
+++ src/components/CakeScene/CakeScene.jsx
@@ -1,9 +1,7 @@
 export default function CakeScene() {
   return (
-    <div className="h-screen w-full bg-black relative overflow-hidden">
-      <div className="absolute inset-0 bg-gradient-to-b from-black via-[#050505] to-black" />
-
-      <Table />
+    <div className="h-screen w-full bg-red-600 flex items-center justify-center">
+      <h1 className="text-white text-4xl">CAKE SCENE IS HERE</h1>
     </div>
   );
 }
